7625374c87eac172d7a824859633252aa47c9970
ryanzola <ryanzola@me.com>
2022-04-12 19:59:44 -0400

    there is an orb

--- src/Experience/Canvas.js
+++ src/Experience/Canvas.js
@@ -13,9 +13,30 @@ export default class Canvas
         this.scene = this.experience.scene
         this.config = this.experience.config
 
+        this.width = this.config.width
+        this.height = this.config.height
+
         this.setGeometry()
         this.setMaterial()
         this.setMesh()
+        this.resize()
+    }
+
+    resize() {
+      this.width = this.config.width
+      this.height = this.config.height
+      this.imageAspect = 1;
+
+      let a1, a2
+      if(this.height / this.width > this.imageAspect) {
+        a1 = (this.width / this.height) * this.imageAspect
+        a2 = 1
+      } else {
+        a1 = 1
+        a2 = (this.height / this.width) / this.imageAspect
+      }
+
+      this.material.uniforms.iResolution.value = new THREE.Vector4(this.width, this.height, a1, a2)
     }
 
     setGeometry() {
--- src/Experience/shaders/canvas/fragment.glsl
+++ src/Experience/shaders/canvas/fragment.glsl
@@ -1,9 +1,60 @@
-uniform vec3 iResolution;
+uniform vec4 iResolution;
+
+varying vec2 vUv;
+
+float sphere(vec3 p) {
+  return length(p) - 0.5;
+}
+
+float scene(vec3 p) {
+  return sphere(p);
+}
+
+vec3 getNormal(vec3 p) {
+  vec2 o = vec2(0.001, 0.0);
+
+  return normalize(
+    vec3(
+      scene(p + o.xyy),
+      scene(p + o.yxy),
+      scene(p + o.yyx)
+    )
+  );
+}
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-  vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
+  vec2 uv = (vUv - vec2(0.5)) * iResolution.zw + vec2(0.5);
+  vec2 p = uv - vec2(0.5);
 
-  fragColor = vec4(uv, 1.0, 1.0);
+  float bw = step(uv.x, 0.5);
+
+  vec3 color = vec3(0.0);
+
+  vec3 camPos = vec3(0.0, 0.0, 2.0);
+  vec3 ray = normalize(vec3(p, -1.0));
+
+  vec3 rayPos = camPos;
+  float currDist = 0.0;
+  float rayLen = 0.0;
+
+  vec3 light = vec3(-1.0, 1.0, 1.0);
+
+  for(int i = 0; i <= 64; i++) {
+    currDist = scene(rayPos);
+    rayLen += currDist;
+
+    rayPos = camPos + ray * rayLen;
+
+    if(abs(currDist) < 0.0001) {
+      vec3 n = getNormal(rayPos);
+
+      float diff = dot(n, light);
+
+      color = vec3(diff, 0.0, 0.0);
+    }
+  }
+
+  fragColor = vec4(color, 1.0);
 }
 
 void main() {
